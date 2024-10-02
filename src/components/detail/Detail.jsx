f05b0404979370ce44204618113ccade5be87db8
Jansen Comadena <jcomadena@esri.com>
2024-10-02 11:15:38 -0700

    basic design completed

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -3,7 +3,62 @@ import "./detail.css"
 const Detail = () => {
     return (
         <div className="detail">
-            Detail
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Jane Doe</h2>
+                <p>Example example example</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & Help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Photos</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://techcrunch.com/wp-content/uploads/2019/07/tennis-game.png" alt=""/>
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://techcrunch.com/wp-content/uploads/2019/07/tennis-game.png" alt=""/>
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://techcrunch.com/wp-content/uploads/2019/07/tennis-game.png" alt=""/>
+                                <span>photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+                <button className="logout">Logout</button>
+            </div>
         </div>
     )
 }
